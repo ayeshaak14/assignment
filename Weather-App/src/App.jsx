4e0f9001a557d471f5c6ef48a92007992329afcf
ayeshaak14 <ak1412844@gmail.com>
2025-03-07 17:42:33 +0530

    Initial commit

---
+++ Weather-App/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Weather from './Components/Weather'
+import Footer from './Components/Footer/Footer'
+
+const App = () => {
+  return (
+    <div className='app'>
+      <Weather/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
